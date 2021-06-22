1a6c43033a9d29807305b3977b2d7256a9ebfdb3
Oscarkindvall <62562054+Oscarkindvall@users.noreply.github.com>
2021-06-22 18:35:23 +0200

    Felaktigt test
    - förväntar sig "wow" --> får 42

--- src/stack.js
+++ src/stack.js
@@ -7,7 +7,7 @@ exports.push = function (x) {
     stack.push(x);
 };
 
-// Returnerar det översta elementet i stacken och tar bort det
+// Returnerar det översta elementet i stacken 
 exports.pop = function () {
     return stack.pop();
 
--- tests/stack.test.js
+++ tests/stack.test.js
@@ -22,9 +22,9 @@ test('peek on stack with two or more elements returns the top element', () => {
     expect(stack.peek()).toBe(42);
 });
 
-test('pop on stack with two or more elements remove last added (top element) and returns that element', () => {
+test('pop on stack with two or more elements and returns that element', () => {
   stack.push(1);
   stack.push("wow");
   stack.push(42);
-  expect(stack.pop()).toBe(42);
+  expect(stack.pop()).toBe("wow");
 });
